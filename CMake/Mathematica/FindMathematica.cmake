1a7172f3e60c86c9aaef68773766c1692fd4a318
Sascha Kratky <skratky@gmail.com>
2018-03-29 20:35:24 +0200

    start 3.2.5

--- CMake/Mathematica/FindMathematica.cmake
+++ CMake/Mathematica/FindMathematica.cmake
@@ -34,7 +34,7 @@ cmake_minimum_required(VERSION 2.8.12)
 cmake_policy(POP)
 
 set (Mathematica_CMAKE_MODULE_DIR "${CMAKE_CURRENT_LIST_DIR}")
-set (Mathematica_CMAKE_MODULE_VERSION "3.2.4")
+set (Mathematica_CMAKE_MODULE_VERSION "3.2.5")
 
 # activate select policies
 if (POLICY CMP0025)
